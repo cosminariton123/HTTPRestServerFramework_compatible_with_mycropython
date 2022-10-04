8082fa0ed8768b11a21b3b941ab12ce029a41531
Darkrider123 <cosmin.ariton@gmail.com>
2022-10-04 11:59:04 +0300

    Refactoring

--- flash_later_to_be_called_main.py
+++ flash_later_to_be_called_main.py
@@ -2,7 +2,6 @@ from my_framework.my_http.http_handler import HttpHandler
 from my_framework.my_socketserver.my_socketserver import SocketServer
 from config import SSID, WLAN_KEY
 
-
 import sys
 
 if sys.implementation.name == "micropython":
